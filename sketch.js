1f85a5a9d6ee231b4bf96617ae3941604bac7b2c
Melissaalves13 <149429317+Melissaalves13@users.noreply.github.com>
2024-03-15 07:56:44 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,48 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(600, 600);
+   background(color(300,5,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+ 
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,20);
+ 
+ 
+ 
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+ 
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+ 
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+   
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+ 
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+   
+}
+
+
+
+
+
+
+
